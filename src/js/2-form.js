4000b7330a265edb67d3a40761e2008112eb673f
Taty <taty.bystrova@gmail.com>
2025-03-04 20:05:32 +0200

    finsh form tasc-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
-const formData = { email: "", message: "" };
+const formData = {email: "", message: ""};
+
+const form = document.querySelector(".feedback-form");
+
+document.addEventListener("contentLoader", () => {
+    const savedData = JSON.parse(localStorage.getItem("feedback-msg"));
+    if (savedData) {
+        formData.email = savedData.email;
+        formData.message = savedData.message;
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+});
+form.addEventListener("input", (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+        formData.email = value.trim();
+    } else if (name === "message") {
+        formData.message = value.trim();
+    }
+    localStorage.setItem("feedback-msg", JSON.stringify(formData));
+});
+ 
+form.addEventListener("submit", (event) => { 
+    event.preventDefault(); 
+    console.log(formData); 
+    form.reset(); 
+    localStorage.removeItem("feedback-msg");
+  
+});
